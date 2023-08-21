4787c1ff6d07cf7824ab291be6f1355335cf0468
alokhandwala94@gmail.com <alokhandwala94@gmail.com>
2023-08-21 15:36:01 +0530

    build fixes

--- src/components/WalletDetails.tsx
+++ src/components/WalletDetails.tsx
@@ -3,7 +3,7 @@ import { Col, Row } from "reactstrap";
 import { WalletContext } from "../context/WalletContext";
 import formatAddress from "@/utils/formatAddress";
 const WalletDetails = () => {
-  const { address, tokenBal, bal } = useContext(WalletContext);
+  const { address } = useContext(WalletContext);
   return (
     <>
       <Row>
@@ -12,22 +12,6 @@ const WalletDetails = () => {
         </Col>
         <Col sm={6} className="value">
           {formatAddress(address)}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={6} className="heading">
-          Ether Balance:
-        </Col>
-        <Col sm={6} className="value">
-          {bal?.substring(0, 8)}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={6} className="heading">
-          DAI Balance:
-        </Col>
-        <Col sm={6} className="value">
-          {tokenBal}
         </Col>
       </Row>
     </>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import { FMT_NUMBER } from "web3";
+import { FMT_BYTES, FMT_NUMBER } from "web3";
 import { DAI_TOKEN, ERC20_ABI, web3Http } from "./consts";
 import { createTransactionObjectByEvent } from "./txns";
 
@@ -9,28 +9,28 @@ export const fetchTransactionList = async (
 
   const currentBlock = await web3Http.eth.getBlockNumber({
     number: FMT_NUMBER.STR,
-    bytes: FMT_NUMBER.HEX
+    bytes: FMT_BYTES.HEX
   });
   const DaiContract = new web3Http.eth.Contract(ERC20_ABI, DAI_TOKEN.address);
 
   let fromBlock = Math.max(parseInt(currentBlock) - 50000, 0)
   let toBlock = currentBlock
   while (fromBlock > 0) {
-    const toEvents = await DaiContract.getPastEvents('Transfer', {
+    const toEvents = await DaiContract.getPastEvents('allEvents', {
       filter: { to: address },
       fromBlock,
       toBlock
     })
-    const fromEvents = await DaiContract.getPastEvents('Transfer', {
+    const fromEvents = await DaiContract.getPastEvents('allEvents', {
       filter: { from: address },
       fromBlock,
       toBlock
     })
     let events = []
     events.push(...toEvents, ...fromEvents)
-    toBlock = Math.max(parseInt(fromBlock) - 1, 0)
+    toBlock = Math.max(fromBlock - 1, 0).toString()
     fromBlock = Math.max(parseInt(toBlock) - 50000, 0)
-    addTransactions(events.map((item: any) => createTransactionObjectByEvent(item)))
+    addTransactions(events.filter(event => event.topics.length == 3).map((item: any) => createTransactionObjectByEvent(item)))
   }
 
   return false;
